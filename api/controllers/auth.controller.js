ca13217f69d45b0a2f563dbca1ad0652e9d15214
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-02-04 22:20:40 +0545

    changes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
-
+ 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
