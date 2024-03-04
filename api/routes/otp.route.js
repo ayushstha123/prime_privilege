4e6abf1396774222d22d30a4833d54fb9140592f
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-03-04 17:24:44 +0545

    Merge changes from main into ayuBranch

--- api/routes/otp.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import otpController from '../controllers/otp.controller.js'; // Adjust the path based on your project structure
-
-const router = express.Router();
-
-router.post('/send-otp', otpController.sendOTP);
-
-export default router;
