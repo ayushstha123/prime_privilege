1db4d1cc4f11ec1ec9edc0ff13f36957ab706536
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-03-04 17:49:41 +0545

    add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -68,7 +68,7 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, 'User not found'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id,role:validUser.role }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
     const expiryDate = new Date(Date.now() + 3600000); // 1 hour
     res
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -44,7 +44,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['visitor', 'student', 'shopkeeper', 'admin'],
+      enum: ['user', 'student', 'shopkeeper', 'admin'],
       default: 'visitor', 
     },
   },
